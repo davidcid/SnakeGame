2c5b2c10440aff8c54acd02efcdbe444a0cf432b
David Cid <david.cid@gmail.com>
2019-03-01 16:18:09 +0100

    improved moving functions

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+const width = 600;
+const height = 400;
+
+const board = document.querySelector("#board");
 const snake = document.querySelector("#snake");
 let snakeColor = getComputedStyle(snake).backgroundColor;
 let snakeX = getComputedStyle(snake).left;
-let lft = 20;
-const width = 600;
+let snakeSize = 20;
+let snakePosition = [0,0];
 
 function cambiarColor() {
 	if (snakeColor === "yellow") {
@@ -14,14 +18,45 @@ function cambiarColor() {
 	}
 }
 
+function newGame() {
+	board.style.width = `${width}px`;
+	board.style.height = `${height}px`;
+}
 
-
-function moveAround() {
-	snake.style.left = `${lft}px`;
-	lft += 20;
-	if (lft >= width) {
-		lft = 0;
+function moveRight() {
+	snake.style.left = `${snakePosition[0] * snakeSize}px`;
+	snakePosition[0] += 1;
+	console.log(snakePosition);
+	if (snakePosition[0] * snakeSize >= width) {
+		snakePosition[0] = 0;
 	}
 }
 
-setInterval(moveAround, 100);
+function moveLeft() {
+	snake.style.left = `${snakePosition[0] * snakeSize}px`;
+	snakePosition[0] -= 1;
+	console.log(snakePosition);
+	if (snakePosition[0] * snakeSize < 0) {
+		snakePosition[0] = width / snakeSize;
+	}
+}
+
+function moveDown() {
+	snake.style.top = `${step}px`;
+	step += 20;
+	if (step >= height) {
+		step = 0;
+	}
+}
+
+function moveUp() {
+	snake.style.top = `${step}px`;
+	step -= 20;
+	if (step < 0) {
+		step = height + step;
+	}
+}
+
+
+newGame();
+setInterval(moveLeft, 1000);
